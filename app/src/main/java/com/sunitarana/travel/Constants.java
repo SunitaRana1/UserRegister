10e1208da9b98d43033bc44d34fc1a7f66f65c0f
Sunita <ranasunita899@gmail.com>
2021-03-21 15:16:36 +0545

    signup code

---
+++ app/src/main/java/com/sunitarana/travel/Constants.java
@@ -0,0 +1,6 @@
+package com.sunitarana.travel;
+
+public class Constants {
+    private static final String ROOT_URL = "http://192.168.254.93/travel/";
+    public static final String URL_REGISTER = ROOT_URL + "register.php";
+}
